bac676c0af8f36c09eafc3e8594f6d8e0b9cc210
Pam Cairo <pcairo_90@outlook.com>
2020-01-20 17:26:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,13 +15,22 @@ class String
   end
 
   def count_sentences
-    self.split([/.?!/]).reject {|x| x.empty?.size
+    self.split(/[.?!]/).reject {|x| x.empty?.size
   end
   
-  def count_sentences_words
-    self.split([/.?!/]).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| == false}.size
-  end
+    def count_sentences_words
+      self.split(/[.?!]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| == false}.size
+   end
 
 end
 
 
+def count_sentences
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+  end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+  end
+
+end

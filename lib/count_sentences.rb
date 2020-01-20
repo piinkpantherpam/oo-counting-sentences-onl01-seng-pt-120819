9bbc747be6417464cbada9e08229b52cb99514c2
Pam Cairo <pcairo_90@outlook.com>
2020-01-20 17:28:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,17 +14,6 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences
-    self.split(/[.?!]/).reject {|x| x.empty?.size
-  end
-  
-    def count_sentences_words
-      self.split(/[.?!]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| == false}.size
-   end
-
-end
-
-
 def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
